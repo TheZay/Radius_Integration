9f695446c669c327160572791fbc675e9421479b
nk79531 <nkeller@choctawnation.com>
2024-01-19 10:08:01 -0600

    added error handling to config_manager.py module

--- src/macollector/config_manager.py
+++ src/macollector/config_manager.py
@@ -2,6 +2,11 @@
 """Handles loading  and parsing the configuration file."""
 import json
 import os.path
+import logging
+
+# Shared logger
+logger = logging.getLogger('macollector')
+
 
 def load_config(file_path: str = 'configs\\config.json') -> dict:
     """
@@ -16,7 +21,13 @@ def load_config(file_path: str = 'configs\\config.json') -> dict:
         dict: The loaded configuration as a dictionary.
     """
     if not os.path.exists(file_path):
-        print(f'Configuration file {file_path} not found. Using default '
-              f'settings.')
-    with open(file_path, 'r', encoding="utf-8") as file:
-        return json.load(file)
+        logger.warning('Configuration file %s not found. Using default '
+                       'settings.', file_path)
+        return {}  # Return a default configuration dictionary
+
+    try:
+        with open(file_path, 'r', encoding="utf-8") as file:
+            return json.load(file)
+    except json.JSONDecodeError as e:
+        logger.error('Error parsing configuration file %s: %s', file_path, e)
+        return {}  # Return a default configuration dictionary
